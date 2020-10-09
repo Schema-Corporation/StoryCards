d9bedc6497db50bfc21da65be013f4872c2dc598
Victor Alonzo Baldeon Rodriguez <u201413960@upc.edu.pe>
2020-10-09 04:09:53 -0500

    Register View
    Primera version de la vista registro. Esta sujeta a cambios :)

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -8,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterPage implements OnInit {
 
   public step: number = 1;
-
+  public showPassword: boolean = false;
+  public showConfirmPassword: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
+  getPasswordType() {
+    return this.showPassword ? 'text' : 'password'
+  }
+  getConfirmPasswordType(){
+    return this.showConfirmPassword ? 'text' : 'password'
+  }
+  toggleTextPassword() {
+    this.showPassword = this.showPassword == true ? false : true;
+  }
+  toggleTextConfirmPassword(){
+    this.showConfirmPassword = this.showConfirmPassword == true ? false : true;
 
+  }
+  addStep(){
+    this.step++;
+  }
 }
