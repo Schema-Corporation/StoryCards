7f7f41686c8c809fac2051d4a156d435794a3ab8
Marcelo Rios <mrios@bevertec.com>
2020-11-02 12:27:24 -0500

    Fixed url of services for deployment purposes

--- src/common/constants/apiUrls.ts
+++ src/common/constants/apiUrls.ts
@@ -1,5 +1,5 @@
-//const BASE_API_URL = "https://pharmaapp-services-be.com"
-const BASE_API_URL = "http://192.168.1.19:3000"
+const BASE_API_URL = "https://pharmaapp-services-be.com"
+// const BASE_API_URL = "http://192.168.1.19:3000"
 export const apiUrls = {
   BASE_API_URL,
   AUTH: `${BASE_API_URL}/login`,
