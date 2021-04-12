de1583c121e40b5f8e337e95584a47a81233360a
Marcelo Rios <mrios@bevertec.com>
2021-04-12 10:51:59 -0500

    Added excel format to download analysis at the end of the role-playing game

--- src/app/mode/role-play/scores/scores.page.ts
+++ src/app/mode/role-play/scores/scores.page.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { NgxIndexedDBService } from 'ngx-indexed-db';
+import { AnswerService } from 'src/app/services/answer/answer.service';
 import { LoginService } from 'src/app/services/auth/login.service';
 import { RolePlayingGuestService } from 'src/app/services/role-playing/role-playing-guest/role-playing-guest.service';
 import { ScoreService } from 'src/app/services/score/score.service';
+import { ExcelService } from 'src/app/util/ExcelService';
 
 @Component({
   selector: 'app-scores',
@@ -19,11 +21,16 @@ export class ScoresPage implements OnInit {
   public role: any;
   public showFinishGameButton: boolean = false;
 
+  public allAnswers = [];
+  public allChallenges = [];
+
   constructor(public navCtrl: NavController,
     public dbService: NgxIndexedDBService,
     public _scoresService: ScoreService,
+    public _answerServices: AnswerService,
     public _loginService: LoginService,
     public _rolePlayingGuestService: RolePlayingGuestService,
+    public _excelService: ExcelService,
     public route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -31,6 +38,18 @@ export class ScoresPage implements OnInit {
       this.gameId = (params["gameId"]).replace(/"/g, '');
       this.getParticipantScores(this.gameId);
       this.getRole();
+      this.getAllChallenges(this.gameId);
+    });
+  }
+
+  getAllChallenges(gameId) {
+    this.dbService.getByIndex('variables', 'name', 'token').subscribe(
+      token => {
+        this.getChallenges(gameId, token.value.token);
+      },
+      error => {
+        this.closeSession();
+        console.log('error: ', error);
     });
   }
 
@@ -115,6 +134,7 @@ export class ScoresPage implements OnInit {
     this.dbService.getByIndex('variables', 'name', 'token').subscribe(
       token => {
         this._scoresService.getScores(gameId, token.value.token).subscribe(answers => {
+          this.allAnswers = answers;
           console.log('scores: ', answers);
           const participants = {};
           this.getScoresFromAnswers(answers);
@@ -156,6 +176,47 @@ export class ScoresPage implements OnInit {
       return b.finalScore - a.finalScore;
     });
     this.participantScores = participantScores;
+  }
+
+  getChallenges(gameId, token) {
+    this._answerServices.getChallenges(gameId, token).subscribe(
+      challenges => {
+        console.log('challenges: ', challenges);
+        this.allChallenges = challenges.filter(x => x.status == 1);
+      },
+      error => {
+        this.closeSession();
+        console.log('error: ', error);
+    });
+  }
+
+  downloadAnalysis() {
+    const aoaData = [
+      ['', 'ROLEPLAYING GAME 6D - RETOS'],
+      ['', 'SOLUCIONES PLANTEADAS A LOS RETOS PROPUESTOS'],
+      ['', '']
+    ];
+    for (var i = 0; i < this.allChallenges.length; i++) {
+      aoaData.push(['Reto ' + (i + 1), this.allChallenges[i].challengeBody]);
+      const answersToChallenge = this.allAnswers.filter(x => x.challengeId == this.allChallenges[i].challengeId).sort((a, b) => {
+        if (a.fullName > b.fullName) {
+          return 1;
+        }
+        else {
+          return -1;
+        }
+      });
+      for (var j = 0; j < answersToChallenge.length; j++) {
+        aoaData.push(['', '']);
+        aoaData.push(['Participante ' + (j + 1), answersToChallenge[j].fullName]);
+        aoaData.push(['', answersToChallenge[j].answerText]);
+      }
+      aoaData.push(['', '']);
+    }
+    console.log('allAnswers: ', this.allAnswers);
+    console.log('allChallenges: ', this.allChallenges);
+    
+    this._excelService.exportAsExcelFile(aoaData, 'SistemaD6');
   }
 
   closeSession() {
